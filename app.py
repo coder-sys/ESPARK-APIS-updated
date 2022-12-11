6b7df6ed65e655eb8ed9f126f357e70e15532d86
ecsbeats <adamb@csxlabs.org>
2022-12-10 18:50:15 -0800

    fix smth

--- app.py
+++ app.py
@@ -738,4 +738,4 @@ def monthly_fee():
             count += 1
     return {'data':count*10,'data_str':str(count*10)}
 if __name__=='__main__':
-    app.run(debug=True, host="localhost")
+    app.run()
